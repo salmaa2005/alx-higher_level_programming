96d834c04d73c22b0129cb0770847c58988af457
salmaa2005 <salmaamostafa2005@gmail.com>
2024-06-01 15:28:48 +0300

    fix 4-print_hexa.py V0.2

--- 0x01-python-if_else_loops_functions/4-print_hexa.py
+++ 0x01-python-if_else_loops_functions/4-print_hexa.py
@@ -1,8 +1,3 @@
 #!/usr/bin/python3
-hexa[]
-mod
-while (num != 0)
-    mod=num%16
-    hexa += mod
-    num = num // 16
-print("0x".format(hexa[::-1]))
+for i in range(0, 99):
+    print("{} = {}".format(i, hex(i)))
